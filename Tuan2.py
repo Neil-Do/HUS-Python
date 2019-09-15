5ad9896ef022406e038599629e9541db3919c9d0
Neil-Do <dotatthanhk60@gmail.com>
2019-09-16 00:53:56 +0700

    Finish bai 8, 9

--- Tuan2.py
+++ Tuan2.py
@@ -1,3 +1,7 @@
+from random import randint
+
+
+
 def inputBai1():
     print("Bai 1. Nhap vao mot day n so nguyen, viet ham kiem tra tinh chat cua day. Xin moi nhap so cac so nguyen n:")
     n = 0
@@ -179,21 +183,215 @@ def bai5():
     pass
 
 from random import randint
-def 
-def bai6():
 
+def smallestInRow(arr):
+    # arr := square matrix
+    arr_smallestInRow = []
+    for r in range(len(arr)):
+        smallest = arr[r][0]
+        for e in arr[r]: #element in row r
+            if e < smallest:
+                smallest = e
+        arr_smallestInRow.append(smallest)
+    print("smallest in row: ")
+    print(arr_smallestInRow)
+    return arr_smallestInRow
+
+
+def greatestInCol(arr):
+    # arr := square matrix
+    arr_greatestInCol = []
+    for c in range(len(arr)):
+        col_c = [row[c] for row in arr]
+        greatest = col_c[0]
+        for e in col_c: # element in col c
+            if e > greatest:
+                greatest = e
+        arr_greatestInCol.append(greatest)
+    print("greatest in column")
+    print(arr_greatestInCol)
+    return arr_greatestInCol
+
+
+def matrixPrinter(mat):
+    for r in mat:
+        print(r)
+
+
+def randomMatrix(r, c):
+    rand_matrix = []
+    for i in range(r):
+        line = []
+        for j in range(c):
+            e = randint(0, 10)
+            line.append(e)
+        rand_matrix.append(line)
+    matrixPrinter(rand_matrix)
+    print()
+    return rand_matrix
+
+
+def randomSquareMatrix(n):
+    return randomMatrix(n, n)
+
+
+def bai6():
+    mat = randomSquareMatrix(5)
+    sml_arr = smallestInRow(mat)
+    grt_arr = greatestInCol(mat)
+    flag = False
+    for c in range(len(grt_arr)):
+        for r in range(len(sml_arr)):
+            if grt_arr[c] == sml_arr[r]:
+                print(r + 1, c + 1)
+                flag = True
+    if flag == False:
+        print("Khong ton tai diem yen ngua.")
+
+
+def congMaTran(a, b):
+    if len(a) != len(b) or len(a[0]) != len(b[0]):
+        print("Ma tran khong cung kich thuoc, khong the cong.")
+        return []
+
+    result_matrix = []
+    for r in range(len(a)):
+        row_r = []
+        for c in range(len(a[0])):
+            row_r.append(a[r][c] + b[r][c])
+        result_matrix.append(row_r)
+    return result_matrix
+
+
+def nhanMaTran(a, b):
+    if len(a[0]) != len(b):
+        print("So cot cua ma tran a khong bang so hang cua ma tran b. Khong the thuc hien phep nhan.")
+        return []
+
+    maTranTich = []
+    for r in range(len(a)):
+        hang = []
+        for c in range(len(b[0])):
+            rc_element = 0
+            a_r = a[r]
+            b_c = [row[c] for row in b]
+            for i in range(len(a_r)):
+                rc_element += a_r[i] * b_c[i]
+            hang.append(rc_element)
+        maTranTich.append(hang)
+    return maTranTich
 
 
 def bai7():
-    pass
+    a = randomMatrix(3, 4)
+    b = randomMatrix(3, 4)
+    c = randomMatrix(4, 2)
+
+    sum_matrix = congMaTran(a, b)
+    print("Tong cua 2 ma tran a va b la: ")
+    matrixPrinter(sum_matrix)
+
+    product_matrix = nhanMaTran(a, c)
+    print("Tich cua 2 ma tran a va c la: ")
+    matrixPrinter(product_matrix)
+
+
+def getMatrixSize(n):
+    matrixSizes = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            matrixSizes.append([i, n // i])
+    return matrixSizes
+
+def createMatrix(matrixSize, matrixElements):
+    # matrixSize := [r, c]
+    e_index = 0
+    mat = []
+    for r in range(matrixSize[0]):
+        mat_row_r = []
+        for c in range(matrixSize[1]):
+            mat_row_r.append(matrixElements[e_index])
+            e_index += 1
+        mat.append(mat_row_r)
+    return mat
 
 
 def bai8():
-    pass
+    print("Bai 8. Nhap vao so nguyen duong n va n so nguyen, in ra ma tran co kich thuoc khac nhau tu n so nguyen da nhap. Xin moi nhap vao so nguyen duong n.")
+    n = int(input("n: "))
+    while n < 1:
+        print("N khong nguyen duong, xin moi nhap lai.")
+        n = int(input("n: "))
+
+    matrixElements = []
+    for i in range(n):
+        matrixElements.append(int(input("Nhap vao so nguyen thu " + str(i + 1) + ": ")))
+
+    matrixSizes = getMatrixSize(n)
+    for i in range(len(matrixSizes)):
+        mat = createMatrix(matrixSizes[i], matrixElements)
+        matrixPrinter(mat)
+
+
+def magicSquareTest(mat):
+    if len(mat) != len(mat[0]):
+        print("Ma tran khong phai ma tran vuong, do do khong the la ma phuong.")
+        return False
+
+    Sum = sum(mat[0])
+
+    # sum of element in row
+    for r in mat:
+        if sum(r) != Sum:
+            return False
+
+    # sum of element in column
+    for c in range(len(mat)):
+        col_c = [row[c] for row in mat]
+        if sum(col_c) != Sum:
+            return False
+
+    # main diagonal
+    sum_of_main_diagonal = 0
+    for i in range(len(mat)):
+        sum_of_main_diagonal += mat[i][i]
+    if sum_of_main_diagonal != Sum:
+        return False
+    # end of main diagonal
+
+    # antidiagonal
+
+    # sum of row index and column index
+    soi = len(mat) - 1
+    sum_of_antidiagonal = 0
+    for i in range(len(mat)):
+        sum_of_antidiagonal += mat[i][soi - i]
+    if sum_of_antidiagonal != Sum:
+        return False
+    # end of antidiagonal
+
+    return True
+
+def magicSquareTestPrint(mat):
+    if magicSquareTest(mat):
+        print("Ma tran vuong da cho la ma phuong.")
+    else:
+        print("Ma tran da cho khong phai la ma phuong.")
 
 
 def bai9():
-    pass
+    magicSquare = [
+        [2, 16, 13, 3],
+        [11, 5, 8, 10],
+        [7, 9, 12, 6],
+        [14, 4, 1, 15]
+    ]
+    matrixPrinter(magicSquare)
+    magicSquareTestPrint(magicSquare)
+    print()
+
+    mat = randomSquareMatrix(4)
+    magicSquareTestPrint(mat)
 
 
 def bai10():
@@ -201,4 +399,4 @@ def bai10():
 
 
 if __name__ == "__main__":
-    bai4()
+    bai8()
---
+++ ex.py
@@ -0,0 +1,7 @@
+from random import randint
+for i in range(4):
+    line = []
+    for j in range(4):
+        n = randint(0, 100)
+        line.append(n)
+    print(line)
