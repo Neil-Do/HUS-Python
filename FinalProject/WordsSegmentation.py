170552d19a0ce73d0d03ce6f42049a206fceaff4
Neil-Do <dotatthanhk60@gmail.com>
2019-12-12 21:01:51 +0700

    Update dongdu data and liblinear python

--- FinalProject/WordsSegmentation.py
+++ FinalProject/WordsSegmentation.py
@@ -1,16 +1,36 @@
+#
+# def accept(Aw, i, j):
+#     return False
+#
+# #V words in sentence
+# def algo1(V):
+#     E = []
+#     n = V.length
+#     for i in range(n):
+#         for j in range(i, n):
+#             if accept(Aw, i, j):
+#                 E.append((i, j+1))
+#     return (tuple(V), tuple(E))
 
-def accept(Aw, i, j):
-    return False
 
-#V words in sentence
-def algo1(V):
-    E = []
-    n = V.length
-    for i in range(n):
-        for j in range(i, n):
-            if accept(Aw, i, j):
-                E.append((i, j+1))
-    return (tuple(V), tuple(E))
+#create vietnamese syllables dictionary
+fVNSyl = open("data/VNsyl.txt")
+vnSyllables = {}
+for line in fVNSyl:
+    line = line.strip()
+    # line == syllable
+    vnSyllables[line] = True
+fVNSyl.close()
+
+
+# create vietnamese wordlist dictionary
+fwordlist = open("data/wordlist.txt")
+wordlist = {}
+for line in fwordlist:
+    line = line.strip()
+    # line == syllable
+    wordlist[line] = True
+fwordlist.close()
 
 
 def fileToSentences(fileName):
@@ -21,3 +41,6 @@ def fileToSentences(fileName):
             line = line.strip()
             sentences.append(re.split("["+ string.punctuation + "]+", line))
     return sentences
+
+
+if __name__ == "__main__":
--- FinalProject/ex.py
+++ FinalProject/ex.py
@@ -2,36 +2,68 @@ import re
 import string
 
 
-def accept(Aw, i, j):
-    return False
-    
+# def accept(Aw, i, j):
+#     return False
+#
+#
+# def algo1(V):
+#     E = []
+#     length = len(V)
+#     for i in range(length):
+#         for j in range(length):
+#             if accept(Aw, i, j):
+#                 E.append((i, j+1))
+#     return (tuple(V), tuple(E))
+#
+#
+# fi = open("in")
+# sentences = []
+# for line in fi:
+#     if line != '\n':
+#         line = line.strip()
+#         sentences.append(re.split("["+ string.punctuation + "]+", line))
+#
+# count = 0
+# for paragraph in sentences:
+#     if count < 10:
+#         for wordGroup in paragraph:
+#             if wordGroup != '':
+#                 # print(wordGroup)
+#                 vertices, edges = algo1(wordGroup)
+#                 for edge in edges:
+#                     first_word = vertices[edge[0]]
+#                     second_word = vertices[edge[1]]
+#                     print(first_word + "_" + second_word)
+#                 count += 1
 
-def algo1(V):
-    E = []
-    length = len(V)
-    for i in range(length):
-        for j in range(length):
-            if accept(Aw, i, j):
-                E.append((i, j+1))
-    return (tuple(V), tuple(E))
+
+#create vietnamese syllables dictionary
+fVNSyl = open("data/VNsyl.txt")
+vnSyllables = {}
+
+for line in fVNSyl:
+    line = line.strip()
+    # line == syllable
+    vnSyllables[line] = True
+fVNSyl.close()
+
+# create vietnamese wordlist dictionary
+fwordlist = open("data/wordlist.txt")
+wordlist = {}
+for line in fwordlist:
+    line = line.strip()
+    # line == syllable
+    wordlist[line] = True
+fwordlist.close()
+
+def test():
+    count = 0
+    for syl in vnSyllables:
+        print(syl)
+        count += 1
+        if count > 10:
+            break
 
 
-fi = open("in")
-sentences = []
-for line in fi:
-    if line != '\n':
-        line = line.strip()
-        sentences.append(re.split("["+ string.punctuation + "]+", line))
-
-count = 0
-for paragraph in sentences:
-    if count < 10:
-        for wordGroup in paragraph:
-            if wordGroup != '':
-                # print(wordGroup)
-                vertices, edges = algo1(wordGroup)
-                for edge in edges:
-                    first_word = vertices[edge[0]]
-                    second_word = vertices[edge[1]]
-                    print(first_word + "_" + second_word)
-                count += 1
+if __name__ == "__main__":
+    test()
